e86188d9bbbe1718e71eed9a435ce733d4fc8421
VadymChan <vadymchan@gmail.com>
2026-05-20 12:14:14 +0300

    add tests for Account Activity Filtering

--- src/ui/pages/account/AccountDetailsPage.js
+++ src/ui/pages/account/AccountDetailsPage.js
@@ -12,11 +12,16 @@ export class AccountDetailsPage {
       name: 'Account Activity',
     });
 
-    const transactionTableRows = page.locator('#transactionTable tbody tr');
-    this.dateCells = transactionTableRows.locator('td:nth-child(1)');
-    this.transactionCells = transactionTableRows.locator('td:nth-child(2)');
-    this.debitCells = transactionTableRows.locator('td:nth-child(3)');
-    this.creditCells = transactionTableRows.locator('td:nth-child(4)');
+    this.transactionTableRows = page.locator('#transactionTable tbody tr');
+    this.dateCells = this.transactionTableRows.locator('td:nth-child(1)');
+    this.transactionCells =
+      this.transactionTableRows.locator('td:nth-child(2)');
+    this.debitCells = this.transactionTableRows.locator('td:nth-child(3)');
+    this.creditCells = this.transactionTableRows.locator('td:nth-child(4)');
+
+    this.activityPeriod = page.locator('#month');
+    this.transactionType = page.locator('#transactionType');
+    this.goButton = page.getByRole('button', { name: 'Go' });
   }
 
   async step(title, stepToRun) {
@@ -57,6 +62,24 @@ export class AccountDetailsPage {
     );
   }
 
+  async selectActivityPeriod(month) {
+    await this.step(`Select $'{month} Activity Period'`, async () => {
+      await this.activityPeriod.selectOption(month);
+    });
+  }
+
+  async selectTransactionType(type) {
+    await this.step(`Select '${type}' Transaction Type`, async () => {
+      await this.transactionType.selectOption(type);
+    });
+  }
+
+  async clickGoButton() {
+    await this.step(`Click 'Go' button`, async () => {
+      await this.goButton.click();
+    });
+  }
+
   async assertAccountIdHasText(accountId) {
     await this.step(`Assert Account ID=${accountId} is shown`, async () => {
       await expect(this.accountId).toHaveText(accountId);
@@ -88,5 +111,75 @@ export class AccountDetailsPage {
     await this.step(`Assert Account Activity banner is shown`, async () => {
       await expect(this.accountActivity).toBeVisible();
     });
+  }
+
+  async assertAccountActivityFiltering(activityPeriod, transactionType) {
+    await this.step(
+      `Assert Account Activity filters transactions by activityPeriod='${activityPeriod}, transactionType='${transactionType}'`,
+      async () => {
+        await this.assertActivityPeriodRowsAreVisible(activityPeriod);
+        await this.assertTransactionTypeRowsAreVisible(transactionType);
+      },
+    );
+  }
+
+  async assertActivityPeriodRowsAreVisible(activityPeriod) {
+    await this.step(
+      `Assert Account Activity shows rows with activityPeriod='${activityPeriod}'`,
+      async () => {
+        const monthToNumeric = {
+          January: '01',
+          February: '02',
+          March: '03',
+          April: '04',
+          May: '05',
+          June: '06',
+          July: '07',
+          August: '08',
+          September: '09',
+          October: '10',
+          November: '11',
+          December: '12',
+        };
+        const monthNumeric = monthToNumeric[activityPeriod];
+
+        if (activityPeriod === 'All') {
+          return;
+        }
+
+        const cells = await this.dateCells.all();
+        for (const cell of cells) {
+          await expect(cell).toHaveText(new RegExp(`^${monthNumeric}-`));
+        }
+      },
+    );
+  }
+
+  async assertTransactionTypeRowsAreVisible(transactionType) {
+    await this.step(
+      `Assert Account Activity shows rows with transactionType='${transactionType}'`,
+      async () => {
+        if (transactionType === 'All') {
+          return;
+        }
+
+        let cells;
+
+        switch (transactionType) {
+          case 'Debit':
+            cells = this.debitCells;
+            break;
+          case 'Credit':
+            cells = this.creditCells;
+            break;
+          default:
+            throw new Error(`Unknown transactionType: ${transactionType}`);
+        }
+
+        for (const cell of await cells.all()) {
+          await expect(cell).not.toBeEmpty();
+        }
+      },
+    );
   }
 }
--- tests/account/accountActivity.spec.js
+++ tests/account/accountActivity.spec.js
@@ -2,8 +2,16 @@ import { test } from '../_fixtures/fixtures';
 import { signUpUser } from '../../src/ui/actions/auth/signUpUser';
 import * as allure from 'allure-js-commons';
 
+const testParameters = [
+  { activityPeriod: 'All', transactionType: 'All' },
+  { activityPeriod: 'May', transactionType: 'All' },
+  { activityPeriod: 'May', transactionType: 'Debit' },
+  { activityPeriod: 'All', transactionType: 'Debit' },
+  { activityPeriod: 'All', transactionType: 'Credit' },
+];
+
 test.describe(`Account activity`, () => {
-  test(`Account activity heading is shown after sign up`, async ({
+  test(`Shows Account activity heading after sign up`, async ({
     accountDetailsPage,
     signedUpUserWithOneAccount,
   }) => {
@@ -12,5 +20,25 @@ test.describe(`Account activity`, () => {
     const { accountData } = signedUpUserWithOneAccount;
     await accountDetailsPage.open(accountData.accountId);
     await accountDetailsPage.assertAccountActivityHeadingIsVisible();
+  });
+
+  testParameters.forEach(({ activityPeriod, transactionType }) => {
+    test(`Filters Account Activity by activityPeriod='${activityPeriod}', transactionType='${transactionType}'`, async ({
+      signedUpUserWithTwoAccounts,
+      accountDetailsPage,
+    }) => {
+      const accountData = signedUpUserWithTwoAccounts;
+      const firstAccount = accountData[0];
+      await accountDetailsPage.open(firstAccount.accountId);
+
+      await accountDetailsPage.selectActivityPeriod(activityPeriod);
+      await accountDetailsPage.selectTransactionType(transactionType);
+      await accountDetailsPage.clickGoButton();
+
+      await accountDetailsPage.assertAccountActivityFiltering(
+        activityPeriod,
+        transactionType,
+      );
+    });
   });
 });
